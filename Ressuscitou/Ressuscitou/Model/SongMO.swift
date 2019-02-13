eb849966c3f5c822856c35156764dee30b99797b
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-02-13 19:42:30 -0200

    Add song entity

---
+++ Ressuscitou/Ressuscitou/Model/SongMO.swift
@@ -0,0 +1,13 @@
+//
+//  SongMO.swift
+//  Ressuscitou
+//
+//  Created by Tiago Maia Lopes on 13/02/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+/// The song entity.
+class SongMO: NSManagedObject {}
